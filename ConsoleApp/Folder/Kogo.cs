753aa7f2e42913ed2431257ff44560744d9459d0
Van Darkholme <jija@kaavo>
2020-09-28 22:15:36 +0500

    Sho

---
+++ ConsoleApp/Folder/Kogo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Folder
+{
+    public class Kogo
+    {
+        public void Sho()
+        {
+            Console.WriteLine("Jopa228");
+        }
+    }
+}
