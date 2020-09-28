fdf06b9e072a1dc5e87cb2faee16f5fa5b389813
Алексей Семенов <Leon4ik_1@mail.ru>
2020-09-28 21:48:18 +0500

    Add project files.

---
+++ ConsoleApp/Folder/Jojon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Folder
+{
+    public static class Jojon
+    {
+        public static int Increase(int num)
+        {
+            return ++num;
+        }
+    }
+}
---
+++ UnitTestProject/ConsoleApp/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp.Folder;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, Jojon.Increase(0));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(5, Jojon.Increase(4));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(100, Jojon.Increase(1));
+        }
+    }
+}
