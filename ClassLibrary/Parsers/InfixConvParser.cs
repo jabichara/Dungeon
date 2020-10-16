09942ecbaec85406fb4caae216cff59f21b8ca8b
Van Darkholme <jija@kaavo>
2020-10-17 02:26:22 +0500

    infix parser

---
+++ ClassLibrary/Parsers/InfixConvParser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Parsers
+{
+    public class InfixConvParser
+    {
+        /// <summary>
+        /// Parse from text to expression items.
+        /// </summary>
+        /// <remarks>
+        /// Example: 
+        /// a+b-100
+        /// a=12
+        /// b=13
+        /// Returns: { (Number, 12), (Operation, Plus), (Number, 13), 
+        /// (Operation, Minus), (Number, 100) }
+        /// </remarks>
+        public List<int> Parse(string[] input)
+        {
+
+            throw new Exception();
+        }
+    }
+
+    public class ExpressionItem
+    {
+        ItemType Type { get; set; }
+        double NumberValue { get; set; }
+        Operation OperationType { get; set; }
+        Bracket BraketType { get; set; }
+
+        public ExpressionItem(ItemType type, Operation operation)
+        {
+            Type = type;
+            OperationType = operation;
+        }
+
+        public ExpressionItem(ItemType type, Bracket bracket)
+        {
+            Type = type;
+            BraketType = bracket;
+        }
+
+        public ExpressionItem(ItemType type, double numberValue)
+        {
+            Type = type;
+            NumberValue = numberValue;
+        }
+    }
+
+    public enum ItemType
+    {
+        Number,
+        Bracket,
+        Operation
+    }
+
+    public enum Bracket
+    {
+        LeftBracket,
+        RightBracket
+    }
+
+    public enum Operation
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+        Power,
+        Logarithm,
+        Cosine,
+        Sine,
+        SquareRoot
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClassLibrary.Measurers;
-using ClassLibrary.SortingAlgorithms;
+using ClassLibrary.Algorithms;
 using ClassLibrary.Generators;
 using ConsoleApp.ConsoleViews;
 
