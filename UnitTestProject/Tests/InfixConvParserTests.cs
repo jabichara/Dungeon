faf4e5c2699d98d61b19ecd4fa947d2138d7b039
Van Darkholme <jija@kaavo>
2020-10-18 14:39:11 +0500

    add tests

---
+++ UnitTestProject/Tests/InfixConvParserTests.cs
@@ -0,0 +1,74 @@
+﻿using ClassLibrary.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.Tests
+{
+    [TestClass]
+    public class InfixConvParserTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            /// Example: 
+            /// Input: "a+b-100
+            ///         a=12
+            ///         b=13"
+            /// Returns: { (Number, 12), (Operation, Plus), (Number, 13), 
+            /// (Operation, Minus), (Number, 100) }
+            /// </remarks>
+            InfixConvParser icp = new InfixConvParser();
+            string[] input = new string[] 
+            { 
+                "a+b-100",
+                "a=12",
+                "b=13"
+            };
+            List<ExpressionItem> expected = new List<ExpressionItem>
+            {
+                new ExpressionItem(ItemType.Number, 12),
+                new ExpressionItem(ItemType.Operation, Operation.Plus),
+                new ExpressionItem(ItemType.Number, 13),
+                new ExpressionItem(ItemType.Operation, Operation.Minus),
+                new ExpressionItem(ItemType.Number, 100)
+            };
+            List<ExpressionItem> actual = icp.Parse(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            InfixConvParser icp = new InfixConvParser();
+            string[] input = new string[]
+            {
+                "ln(a)*(b+cos(90))",
+                "a=12",
+                "b=13"
+            };
+            List<ExpressionItem> expected = new List<ExpressionItem>
+            {
+                new ExpressionItem(ItemType.Operation, Operation.Logarithm),
+                new ExpressionItem(ItemType.Bracket, Bracket.LeftBracket),
+                new ExpressionItem(ItemType.Number, 12),
+                new ExpressionItem(ItemType.Bracket, Bracket.RightBracket),
+                new ExpressionItem(ItemType.Operation, Operation.Multiply),
+                new ExpressionItem(ItemType.Bracket, Bracket.LeftBracket),
+                new ExpressionItem(ItemType.Number, 13),
+                new ExpressionItem(ItemType.Operation, Operation.Plus),
+                new ExpressionItem(ItemType.Operation, Operation.Cosine),
+                new ExpressionItem(ItemType.Bracket, Bracket.LeftBracket),
+                new ExpressionItem(ItemType.Number, 90),
+                new ExpressionItem(ItemType.Bracket, Bracket.RightBracket),
+                new ExpressionItem(ItemType.Bracket, Bracket.RightBracket),
+            };
+            List<ExpressionItem> actual = icp.Parse(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- UnitTestProject/Tests/StackOpParserTests.cs
+++ UnitTestProject/Tests/StackOpParserTests.cs
@@ -14,9 +14,43 @@ namespace UnitTestProject.Tests
         [TestMethod]
         public void Test()
         {
+            /// Example: 
+            /// Input: "3 1,56 1,cat" 
+            /// Returns: { (StackCommand.Top, null), 
+            /// (StackCommand.Push, 56), (StackCommand.Push, "cat") }
             StackOperationsParser op = new StackOperationsParser();
-            string[] fdsa = null;
-            op.Parse(fdsa);
+            string[] input = new string[] { "3 1,56 1,cat" };
+            List<StackOperation> expected = new List<StackOperation>
+            {
+                new StackOperation(StackCommand.Top, null),
+                new StackOperation(StackCommand.Push, 56),
+                new StackOperation(StackCommand.Push, "cat")
+            };
+            List<StackOperation> actual = op.Parse(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            StackOperationsParser op = new StackOperationsParser();
+            string[] input = new string[] 
+            { 
+                "4 1,432.543", 
+                "3 2 1,joska 4 5"
+            };
+            List<StackOperation> expected = new List<StackOperation>
+            {
+                new StackOperation(StackCommand.isEmpty, null),
+                new StackOperation(StackCommand.Push, 432.543),
+                new StackOperation(StackCommand.Top, null),
+                new StackOperation(StackCommand.Pop, null),
+                new StackOperation(StackCommand.Push, "joska"),
+                new StackOperation(StackCommand.isEmpty, null),
+                new StackOperation(StackCommand.Print, null)
+            };
+            List<StackOperation> actual = op.Parse(input);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
