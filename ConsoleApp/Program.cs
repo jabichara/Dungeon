cdb0af1fb965185237dffad94ad05c1d5b110101
Evgeniy <djonich@mail.ru>
2020-11-03 21:42:42 +0500

    ROOT

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,8 +19,27 @@ namespace ConsoleApp
         {
             //Console.SetBufferSize(Console.BufferWidth, 32766);
             //MeasureStackOperations.Show();
-            CalculatingInfixToPostfix.Show();
-            Console.ReadKey();
+            //CalculatingInfixToPostfix.Show();
+            //Console.ReadKey();
+            RB tree = new RB();
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(1);
+            tree.Insert(9);
+            tree.Insert(-1);
+            tree.Insert(11);
+            tree.Insert(6);
+            tree.DisplayTree();
+            tree.Delete(-1);
+            tree.DisplayTree();
+            tree.Delete(9);
+            tree.DisplayTree();
+            tree.Delete(5);
+            tree.DisplayTree();
+            Console.WriteLine();
+            Console.WriteLine("Minimum - " + tree.root.data);
+            Console.ReadLine();
         }
     }
 }
