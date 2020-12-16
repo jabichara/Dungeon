82e07ef596fd04569201cb7ea22fbdd81d666581
student <student@DESKTOP-R1I5FKV>
2020-12-16 19:38:54 +0500

    Hash

--- ClassLibrary/Structures/HashTable.cs
+++ ClassLibrary/Structures/HashTable.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ClassLibrary.Structures
 {
 
-    public class HashTable<MovieInfo>
+    public class HashTable
     {
         public long MaxCount { get; set; }
 
@@ -34,22 +34,41 @@ namespace ClassLibrary.Structures
 
         public long GetHash(MovieInfo movie)
         {
-            return 0;
+            long hash = 0;
+            var rnd = new Random();
+            foreach (char c in movie.Name)
+            {
+                hash += Convert.ToInt16(c)*rnd.Next(1000);
+            }
+            foreach (char c in movie.Link)
+            {
+                hash += Convert.ToInt16(c)*rnd.Next(150);
+            }
+            foreach (char c in movie.Format)
+            {
+                hash += Convert.ToInt16(c)*rnd.Next(50);
+            }
+            hash += movie.Size;
+            while (hash > MaxCount)
+                hash /= 2;
+            return hash;
         }
-    }
 
-    public class HashTableItem<T>
-    {
-        public int Index { get; set; }
-        public T Value { get; set; }
-        public T Next { get; set; }
-    }
 
-    public class MovieInfo
-    {
-        string Name { get; set; }
-        string Format { get; set; }
-        long Size { get; set; }
-        string Link { get; set; }
+        public class MovieInfo
+        {
+            public string Name { get; set; }
+            public string Format { get; set; }
+            public long Size { get; set; }
+            public string Link { get; set; }
+
+            public MovieInfo(string name, string link, long size, string format)
+            {
+                Name = name;
+                Link = link;
+                Size = size;
+                Format = format;
+            }
+        }
     }
 }
